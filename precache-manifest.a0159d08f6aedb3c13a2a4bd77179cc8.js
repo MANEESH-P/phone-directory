1584c625beba04a43ec7141e26a595d01c5cabad
MANEESH P <mputhenhouse8@gmail.com>
2019-03-13 21:58:06 +0530

    Updates

---
+++ precache-manifest.a0159d08f6aedb3c13a2a4bd77179cc8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/directory/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "d5c72f239ad12884ff5a",
+    "url": "/directory/static/css/main.3614bfc3.chunk.css"
+  },
+  {
+    "revision": "2ca75f6c5e7579a49034",
+    "url": "/directory/static/js/runtime~main.b5e9c2b1.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/directory/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "152d2a7628c28480c1e5",
+    "url": "/directory/static/js/2.4192ea04.chunk.js"
+  },
+  {
+    "revision": "d5c72f239ad12884ff5a",
+    "url": "/directory/static/js/main.5d784447.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/directory/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/directory/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/directory/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "152d2a7628c28480c1e5",
+    "url": "/directory/static/css/2.bd0ae108.chunk.css"
+  },
+  {
+    "revision": "d06b3e91a9261788efc27eeaa96d0628",
+    "url": "/directory/index.html"
+  }
+];
